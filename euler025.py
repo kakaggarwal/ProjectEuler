7f43583e6cf924af58350fe83b561eb79d2367ef
Krishan Aggarwal <kakaggarwal@gmail.com>
2020-10-31 09:05:12 +0530

    Euler 025 solution updated

--- euler025.py
+++ euler025.py
@@ -5,18 +5,17 @@ factCounts = [1]
 
 
 def getFacts(n):
-    facts.append(facts[-1] + facts[-2])
+    while True:
+        facts.append(facts[-1] + facts[-2])
 
-    if len(str(facts[-1])) == n:
-        factCounts.append(len(facts))
-        return factCounts[-1]
-
-    return getFacts(n)
+        if len(str(facts[-1])) == n:
+            factCounts.append(len(facts))
+            return factCounts[-1]
 
 
 def euler(n):
     if len(factCounts) >= n:
-        return factCounts[n]
+        return factCounts[n - 1]
 
     return getFacts(n)
 
